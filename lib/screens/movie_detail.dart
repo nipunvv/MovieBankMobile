0058581a48ea6334b6524aa5e83edffc11d426e6
Nipun VV <nipunvv@gmail.com>
2021-07-30 23:45:27 +0530

    Add title and genre UI

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_bank_mobile/constants.dart';
 import 'package:movie_bank_mobile/models/movie.dart';
+import 'package:movie_bank_mobile/widgets/genre_list.dart';
 
 class MovieDetail extends StatefulWidget {
   final Movie movie;
@@ -12,21 +14,49 @@ class MovieDetail extends StatefulWidget {
 }
 
 class _MovieDetailState extends State<MovieDetail> {
+  getGenres() {
+    //
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Hero(
-            tag: 'movie_image_${widget.movie.category}_${widget.movie.id}',
-            child: CachedNetworkImage(
-              imageUrl: "$TMDB_WEB_URL/w342${widget.movie.posterPath}",
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.contain,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+              tag: 'movie_image_${widget.movie.category}_${widget.movie.id}',
+              child: CachedNetworkImage(
+                imageUrl: "$TMDB_WEB_URL/w342${widget.movie.posterPath}",
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.contain,
+              ),
             ),
           ),
-        ),
+          Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.5,
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.movie.title,
+                  style: GoogleFonts.barlowCondensed(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white.withOpacity(.85),
+                    letterSpacing: 2,
+                    fontSize: 35,
+                  ),
+                ),
+                GenreList(genreIds: widget.movie.genreIds),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/genre_list.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GenreList extends StatelessWidget {
+  const GenreList({
+    Key? key,
+    required this.genreIds,
+  }) : super(key: key);
+
+  final dynamic genreIds;
+
+  getGenres() {
+    return Container();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (genreIds == null || genreIds.length == 0)
+      return Container();
+    else
+      return Row(
+        children: [
+          for (int id in genreIds)
+            Padding(
+              padding: EdgeInsets.only(
+                right: 10,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                  3,
+                ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xffff4069),
+                        Color(0xffff4551),
+                        Color(0xfffe4a40),
+                      ],
+                    ),
+                  ),
+                  child: Text(
+                    'CRIME',
+                    style: GoogleFonts.barlowCondensed(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white.withOpacity(.85),
+                      letterSpacing: 2,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      );
+  }
+}
