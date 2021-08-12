f5178dbb19885bc3b8f047c8e33d1461d5851af2
Nipun VV <nipunvv@gmail.com>
2021-08-12 23:47:40 +0530

    Create class for serving custom text styles

---
+++ lib/utils/custom_text_styles.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextStyles {
+  static TextStyle text14(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(0.9),
+      letterSpacing: 2,
+      fontSize: 14,
+    );
+  }
+
+  static TextStyle text16(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w800,
+      color: Colors.white.withOpacity(0.9),
+      letterSpacing: 2,
+      fontSize: 16,
+      decoration: TextDecoration.underline,
+    );
+  }
+
+  static TextStyle text30(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(.85),
+      letterSpacing: 2,
+      fontSize: 30,
+    );
+  }
+
+  static TextStyle text20light(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Color(0xffa1a2d2),
+      letterSpacing: 2,
+      fontSize: 20,
+    );
+  }
+
+  static TextStyle text28(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(.85),
+      letterSpacing: 2,
+      fontSize: 28,
+    );
+  }
+
+  static TextStyle text16light(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Color(0xffa1a2d2),
+      letterSpacing: 2,
+      fontSize: 16,
+    );
+  }
+
+  static TextStyle text35(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(.85),
+      letterSpacing: 2,
+      fontSize: 35,
+    );
+  }
+
+  static TextStyle text18(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(.85),
+      letterSpacing: 2,
+      fontSize: 18,
+    );
+  }
+
+  static TextStyle text20(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Colors.white.withOpacity(.85),
+      letterSpacing: 2,
+      fontSize: 20,
+    );
+  }
+
+  static TextStyle text14light(BuildContext context) {
+    return GoogleFonts.barlowCondensed(
+      fontWeight: FontWeight.w500,
+      color: Color(0xffa1a2d2),
+      letterSpacing: 2,
+      fontSize: 14,
+    );
+  }
+}
